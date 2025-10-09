ae18d5d4e659af0d3faf849a69d341fdaa49fc1e
franciscomaqueda <zfmaqueda@gmail.com>
2025-10-09 20:05:55 -0300

    Add files via upload

---
+++ tpfinaldupla/funciones.js
@@ -0,0 +1,41 @@
+function SiguientePantalla(NuevaPantalla){
+ historial.push(pantalla);
+ pantalla = NuevaPantalla;
+}
+
+function VolverPantalla(){
+ if (historial.length > 0){
+  pantalla = historial.pop(); 
+ }
+}
+
+function fondoDeTexto(texto) {
+ textSize (12);
+ let margen = 20;
+ let anchoMax = width * 0.8;
+ let altoTexto = textAscent() + textDescent() + margen * 2;
+ let posicionTextoY = height - altoTexto - 100;
+ fill(255,200);
+ rect(width/2 - anchoMax/2, posicionTextoY + margen, anchoMax, altoTexto + 10, 10);
+ fill(0);
+ textAlign(LEFT, CENTER);
+ text(texto, width/2 - anchoMax/2 + margen, posicionTextoY + margen, anchoMax - margen * 2, altoTexto);
+}
+
+function dibujarBoton(x, y, an, al, texto){
+  if (detectarBoton (x, y, an, al)){
+   fill(70);   
+  } else {
+    fill(135);
+  }
+  rect(x, y, an, al, 10);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text(texto, x + an/2, y + al/2);
+}
+
+function detectarBoton(x, y ,an, al){
+  return mouseX > x && mouseX < x + an && mouseY > y && mouseY < y + al;
+}
+
+  
